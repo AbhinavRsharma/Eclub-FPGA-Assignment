24d938cf290fcc4d9ef6c4e2d2ce0a5f4dbbaf5a
Abhinav Sharma <43822140+AbhinavRsharma@users.noreply.github.com>
2019-07-04 14:50:14 +0530

    Update complex_mulp.v

--- Mathematical_Modelling_of_QFT_in_verilog/complex_mulp.v
+++ Mathematical_Modelling_of_QFT_in_verilog/complex_mulp.v
@@ -19,6 +19,7 @@
 //
 //////////////////////////////////////////////////////////////////////////////////
 module complex_mulp(in_r, in_i, cos_2p_by, sin_2p_by, out_r, out_i
+	
     );
 input wire signed [7:0] in_r;// 1 sign bit, 7 integer bits  
 input wire signed [7:0] in_i;
@@ -32,8 +33,8 @@ input wire signed [11:0] cos_2p_by;
 wire signed [17:0] in_r_temp, in_i_temp; 
 wire signed [17:0] cos, sin;
 wire signed [34:0] out_r_temp1, out_i_temp1, out_r_temp2, out_i_temp2;
-wire signed [35:0] out_r_temp, out_i_temp;
-
+reg signed [35:0] out_r_temp3, out_i_temp3;
+reg signed [12:0] out_r_temp4, out_i_temp4;
 
 assign in_r_temp = {in_r,10'd0};
 assign in_i_temp = {in_i,10'd0};
@@ -47,10 +48,35 @@ assign out_r_temp2 = -sin*in_i_temp;
 assign out_i_temp1 = cos*in_i_temp;
 assign out_i_temp2 = sin*in_r_temp;
 
-assign out_r_temp = out_r_temp1+out_r_temp2;
-assign out_i_temp = out_i_temp1+out_i_temp2;
 
-assign out_r = {out_r_temp[35],out_r_temp[27:17]};
-assign out_i = {out_i_temp[35],out_i_temp[27:17]};
+always @*
+begin
+		out_r_temp3 = out_r_temp1+out_r_temp2;
+		out_i_temp3 = out_i_temp1+out_i_temp2;
+		if(out_r_temp3[35])
+		begin
+			out_r_temp3 = out_r_temp3-1;
+			out_r_temp3 = ~out_r_temp3;
+			out_r_temp4 = out_r_temp3[27:17];
+			out_r_temp4 = -out_r_temp4;
+		end
+		else begin
+			out_r_temp4 = out_r_temp3[27:17];
+		end
+		
+		if(out_i_temp3[35])
+		begin
+			out_i_temp3 = out_i_temp3-1;
+			out_i_temp3 = ~out_i_temp3;
+			out_i_temp3 = out_i_temp3[27:17];
+			out_i_temp4 = ~out_i_temp4;
+		end
+		else begin
+			assign out_i_temp4 = out_i_temp3[27:17];
+		end
+end
+
+assign out_r = out_r_temp4;
+assign out_i = out_i_temp4;
 
 endmodule
